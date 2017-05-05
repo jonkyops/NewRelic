fa53119a8dd9d8c8559f0cb8037f41df41da3a1f
jonkyops <chrisbblake@gmail.com>
2017-05-04 19:22:23 -0500

    cleanup

--- NewRelic/NewRelic.psm1
+++ NewRelic/NewRelic.psm1
@@ -12,17 +12,3 @@ foreach ($Import in @($Public + $Private)) {
         Write-Error -Message "Failed to import function $($Import.fullname): $_"
     }
 }
-
-# Cmdlets and associated data types are defined in PowerShellLoggingModule.dll.  This script file just handles detaching the HostIOInterceptor object when the module unloads.
-# $dllPath = Join-Path -Path $MyInvocation.MyCommand.ScriptBlock.Module.ModuleBase -ChildPath PowerShellLoggingModule.dll
-# try {
-#     Import-Module -Name $dllPath -ErrorAction Stop
-# }
-# catch {
-#     throw
-# }
-
-# $MyInvocation.MyCommand.ScriptBlock.Module.OnRemove = {
-#     [PSLogging.HostIOInterceptor]::Instance.RemoveAllSubscribers()
-#     [PSLogging.HostIOInterceptor]::Instance.DetachFromHost()
-# }
